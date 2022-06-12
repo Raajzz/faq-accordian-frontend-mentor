9547d1598b77b6a2200e3b801cbb700e8823fb69
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-06-12 11:27:33 +0530

    Started working with picture tag and srcset

--- src/components/FaqAccordian.js
+++ src/components/FaqAccordian.js
@@ -2,77 +2,85 @@ import React, { useState } from "react";
 import "./FaqAccordian.css";
 import { qna } from "../assets/qna";
 import iconArrowDown from "../assets/icon-arrow-down.svg";
+import illusWomenDesk from "../assets/desktop/illustration-woman-online-desktop.svg";
+import illusWomenMobile from "../assets/mobile/illustration-woman-online-mobile.svg";
 // import illusMobile from "../assets/mobile/illustration-woman-online-mobile.svg"
 // import bgPatternMobile from "../assets/mobile/bg-pattern-mobile.svg"
 
 const FaqAccordian = () => {
-	const initialArray = Array(qna.length).fill(false);
-	const [showAccordian, setShowAccordian] = useState(initialArray);
+  const initialArray = Array(qna.length).fill(false);
+  const [showAccordian, setShowAccordian] = useState(initialArray);
 
-	console.log(showAccordian);
-	return (
-		<>
-			<div className="card-container">
-				<main className="card">
-					{/* image and other stuff */}
-					<div className="qna-wrapper">
-						<div className="heading">FAQ</div>
+  console.log(showAccordian);
+  return (
+    <>
+      <div className="card-container">
+        <main className="card">
+          {/* image and other stuff */}
+          <div className="image-tag">
+            <picture>
+              <source srcset={illusWomenDesk} media="(min-width: 768px)" />
+              <img src={illusWomenMobile} alt="Illustration of a Women" />
+            </picture>
+          </div>
+          <div className="qna-wrapper">
+            <div className="heading">FAQ</div>
 
-						{/* accordian div */}
-						<div>
-							{qna.map((item, index) => {
-								return (
-									<>
-										<div className="qna-container" key={index}>
-											<div
-												className="question-container"
-												onClick={() => {
-													let allElements = [...showAccordian];
-													allElements[index] = !allElements[index];
-													setShowAccordian(allElements);
-												}}
-											>
-												<p
-													className={`question ${
-														showAccordian[index]
-															? "show-margin-utility-question question-bold"
-															: ""
-													}`}
-												>
-													{item.question}
-												</p>
-												<div
-													className={`down-arrow-container
+            {/* accordian div */}
+            <div>
+              {qna.map((item, index) => {
+                return (
+                  <>
+                    <div className="qna-container" key={index}>
+                      <div
+                        className="question-container"
+                        onClick={() => {
+                          let allElements = [...showAccordian];
+                          allElements[index] = !allElements[index];
+                          setShowAccordian(allElements);
+                        }}
+                      >
+                        <p
+                          className={`question ${
+                            showAccordian[index]
+                              ? "show-margin-utility-question question-bold"
+                              : ""
+                          }`}
+                        >
+                          {item.question}
+                        </p>
+                        <div
+                          className={`down-arrow-container
 												${showAccordian[index] ? "down-arrow-rotate" : ""}
 											`}
-												>
-													<img
-														src={iconArrowDown}
-														alt="Down Arrow Icon"
-														className={`down-arrow-icon`}
-													/>
-												</div>
-											</div>
-											<p
-												className={`answer ${
-													showAccordian[index]
-														? "show-margin-utility-answer"
-														: ""
-												}`}
-											>
-												{showAccordian[index] && item.answer}
-											</p>
-											<hr />
-										</div>
-									</>
-								);
-							})}
-						</div>
-					</div>
-				</main>
-			</div>
-		</>
-	);
+                        >
+                          <img
+                            src={iconArrowDown}
+                            alt="Down Arrow Icon"
+                            className={`down-arrow-icon`}
+                          />
+                        </div>
+                      </div>
+                      <p
+                        className={`answer ${
+                          showAccordian[index]
+                            ? "show-margin-utility-answer"
+                            : ""
+                        }`}
+                      >
+                        {showAccordian[index] && item.answer}
+                      </p>
+                      <hr />
+                    </div>
+                  </>
+                );
+              })}
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default FaqAccordian;
